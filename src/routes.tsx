f4682c02e27bd5189a0d0f14b3dc05e67dc2677f
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-08-18 17:38:54 -0300

    feat: header

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,18 +2,16 @@ import { ThemeProvider } from "styled-components";
 
 import { GlobalStyles } from "./styles/global-styles";
 
-import {
-  BrowserRouter as Router,
-  Routes as Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import { Header } from "./components/Header";
 import { theme } from "./styles/theme";
 
 export const Routes = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
+        <Header />
         <GlobalStyles />
       </ThemeProvider>
     </Router>
